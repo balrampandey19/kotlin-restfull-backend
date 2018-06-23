829c82235d11cd8a4b94dcb1d5f75342f53b97ac
Balram Pandey <balrampandey19@gmail.com>
2018-06-23 23:07:52 +0530

    Env Setup

---
+++ src/main/kotlin/com/balrampandey19/kotlinrestfulbackend/KotlinRestfulBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.balrampandey19.kotlinrestfulbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestfulBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinRestfulBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/balrampandey19/kotlinrestfulbackend/KotlinRestfulBackendApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.balrampandey19.kotlinrestfulbackend
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinRestfulBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
