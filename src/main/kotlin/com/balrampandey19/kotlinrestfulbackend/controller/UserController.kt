25d765dd86ecbdc9c5e7e04ccf46465f7bc6e360
Balram Pandey <balrampandey19@gmail.com>
2018-06-29 16:49:06 +0530

    Get all user

--- src/main/kotlin/com/balrampandey19/kotlinrestfulbackend/controller/UserController.kt
+++ src/main/kotlin/com/balrampandey19/kotlinrestfulbackend/controller/UserController.kt
@@ -1,6 +1,8 @@
 package com.balrampandey19.kotlinrestfulbackend.controller
 
+import com.balrampandey19.kotlinrestfulbackend.model.User
 import com.balrampandey19.kotlinrestfulbackend.repository.UserRepository
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,4 +10,9 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api")
 class UserController(private val userRepository: UserRepository) {
+
+
+    @GetMapping("/articles")
+    fun getAllUser(): List<User> =
+            userRepository.findAll()
 }
