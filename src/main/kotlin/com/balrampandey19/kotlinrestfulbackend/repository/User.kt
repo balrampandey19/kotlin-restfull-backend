895b1ae17c58d01cba5ef8bbcdb0da2080fc62f3
Balram Pandey <balrampandey19@gmail.com>
2018-06-29 16:18:31 +0530

    Creating user model

---
+++ src/main/kotlin/com/balrampandey19/kotlinrestfulbackend/repository/User.kt
@@ -0,0 +1,23 @@
+package com.balrampandey19.kotlinrestfulbackend.repository
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.validation.constraints.NotBlank
+
+@Entity
+data class User(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long = 0,
+
+        @get:NotBlank
+        val name: String = "",
+
+        @get:NotBlank
+        val email: String = "",
+
+        @get:NotBlank
+        val password: String = ""
+)
