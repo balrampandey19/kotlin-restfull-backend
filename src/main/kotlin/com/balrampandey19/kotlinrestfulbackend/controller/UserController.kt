081d75b316ace42307f89d7a97386885cc0a6050
Balram Pandey <balrampandey19@gmail.com>
2018-06-29 16:58:55 +0530

    get user by id

--- src/main/kotlin/com/balrampandey19/kotlinrestfulbackend/controller/UserController.kt
+++ src/main/kotlin/com/balrampandey19/kotlinrestfulbackend/controller/UserController.kt
@@ -2,6 +2,7 @@ package com.balrampandey19.kotlinrestfulbackend.controller
 
 import com.balrampandey19.kotlinrestfulbackend.model.User
 import com.balrampandey19.kotlinrestfulbackend.repository.UserRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
@@ -16,6 +17,14 @@ class UserController(private val userRepository: UserRepository) {
             userRepository.findAll()
 
     @PostMapping("/user")
-    fun createNewArticle(@Valid @RequestBody article: User): User =
-            userRepository.save(article)
+    fun createNewArticle(@Valid @RequestBody user: User): User =
+            userRepository.save(user)
+
+    @GetMapping("/user/{id}")
+    fun getUserById(@PathVariable(value = "id") userId: Long): ResponseEntity<Article> {
+        return userRepository.findById(userId).map { user ->
+            ResponseEntity.ok(user)
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
 }
