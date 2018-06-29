c3992831e29b36269a9005d2241439955aad6f84
Balram Pandey <balrampandey19@gmail.com>
2018-06-29 16:47:59 +0530

    Maping UserRepository in UserController

--- src/main/kotlin/com/balrampandey19/kotlinrestfulbackend/controller/UserController.kt
+++ src/main/kotlin/com/balrampandey19/kotlinrestfulbackend/controller/UserController.kt
@@ -1,4 +1,11 @@
 package com.balrampandey19.kotlinrestfulbackend.controller
 
-class UserController {
+import com.balrampandey19.kotlinrestfulbackend.repository.UserRepository
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/api")
+class UserController(private val userRepository: UserRepository) {
 }
